40ac35b0d70831c0f5d26d71f8a5f38611f24f1b
Tayyab Amin <a_tayyab46@yahoo.com>
2020-09-04 13:22:21 +0500

    bining of farmers with users and field

--- src/app/admin-view-users/admin-view-users.page.ts
+++ src/app/admin-view-users/admin-view-users.page.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+
+import {
+  AngularFirestore,
+  AngularFirestoreCollection,
+} from "@angular/fire/firestore";
 
 @Component({
-  selector: 'app-admin-view-users',
-  templateUrl: './admin-view-users.page.html',
-  styleUrls: ['./admin-view-users.page.scss'],
+  selector: "app-admin-view-users",
+  templateUrl: "./admin-view-users.page.html",
+  styleUrls: ["./admin-view-users.page.scss"],
 })
 export class AdminViewUsersPage implements OnInit {
+  users: any = [];
 
-  constructor() { }
+  constructor(private afs: AngularFirestore) {}
 
   ngOnInit() {
+    const ownerEmail = localStorage.getItem("email");
+    this.afs
+      .collection("users", (ref) => ref.where("ownerEmail", "==", ownerEmail))
+      .valueChanges()
+      .subscribe((_users) => {
+        this.users = _users;
+        console.log(this.users);
+      });
   }
-
 }
