0af1b48626d77d8a286de49ef8232b591775d975
Tayyab Amin <a_tayyab46@yahoo.com>
2020-09-06 21:52:59 +0500

    bit changes but not final

--- src/app/view-farmers/view-farmers.page.ts
+++ src/app/view-farmers/view-farmers.page.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { AngularFirestore } from "@angular/fire/firestore";
 
 @Component({
-  selector: 'app-view-farmers',
-  templateUrl: './view-farmers.page.html',
-  styleUrls: ['./view-farmers.page.scss'],
+  selector: "app-view-farmers",
+  templateUrl: "./view-farmers.page.html",
+  styleUrls: ["./view-farmers.page.scss"],
 })
 export class ViewFarmersPage implements OnInit {
+  fields: any = [];
 
-  constructor() { }
+  constructor(private afs: AngularFirestore) {}
 
   ngOnInit() {
+    this.afs
+      .collection("fields")
+      .valueChanges()
+      .subscribe((fields) => {
+        this.fields = fields;
+        console.log(this.fields);
+      });
   }
-
 }
