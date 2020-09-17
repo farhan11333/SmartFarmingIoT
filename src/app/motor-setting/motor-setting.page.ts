1c61057ac54ab44f7a2ff62da954fd310c351370
Tayyab Amin <a_tayyab46@yahoo.com>
2020-09-18 01:01:48 +0500

    nochanges

--- src/app/motor-setting/motor-setting.page.ts
+++ src/app/motor-setting/motor-setting.page.ts
@@ -1,18 +1,17 @@
-<<<<<<< HEAD
 import { Component, OnInit } from "@angular/core";
-=======
-import { Component, OnInit } from '@angular/core';
 import {
   AngularFirestore,
   AngularFirestoreCollection,
   AngularFirestoreDocument,
-} from '@angular/fire/firestore';
-import { Observable } from 'rxjs';
+} from "@angular/fire/firestore";
+import { Observable } from "rxjs";
 import {
-  FormGroup, FormBuilder, Validators, FormControl, } from '@angular/forms';
+  FormGroup,
+  FormBuilder,
+  Validators,
+  FormControl,
+} from "@angular/forms";
 export interface Auto {
->>>>>>> fc5fca2d104568ef83ffbac55d51b5f7c0d92f41
-
   autoIsrunning: boolean;
 }
 @Component({
@@ -21,70 +20,62 @@ export interface Auto {
   styleUrls: ["./motor-setting.page.scss"],
 })
 export class MotorSettingPage implements OnInit {
-<<<<<<< HEAD
-  constructor() {}
-
-  ngOnInit() {}
-=======
-  constructor(public afs: AngularFirestore, private formBuilder: FormBuilder, ) { }
+  constructor(public afs: AngularFirestore, private formBuilder: FormBuilder) {}
   isChecker: boolean;
   user: any = { farmId: {} };
   validations_form: FormGroup;
-  errorMessage: '';
-  successMessage = '';
+  errorMessage: "";
+  successMessage = "";
   validation_messages = {
-    min: [{ type: 'required', message: 'Minimum value is required.' }],
-    max: [{ type: 'required', message: 'Maximum value is required.' }],
-
+    min: [{ type: "required", message: "Minimum value is required." }],
+    max: [{ type: "required", message: "Maximum value is required." }],
   };
   autoButton: Observable<Auto>;
 
   ngOnInit() {
     this.validations_form = this.formBuilder.group({
-      min: new FormControl('', Validators.compose([Validators.required, Validators.pattern('[0-9]*')])),
-      max: new FormControl('', Validators.compose([Validators.required,
-        Validators.pattern('[0-9]*')])),
-      });
+      min: new FormControl(
+        "",
+        Validators.compose([Validators.required, Validators.pattern("[0-9]*")])
+      ),
+      max: new FormControl(
+        "",
+        Validators.compose([Validators.required, Validators.pattern("[0-9]*")])
+      ),
+    });
 
-
-    this.autoButton = this.afs.collection('devices').doc<Auto>('farmEST17a').valueChanges();
+    this.autoButton = this.afs
+      .collection("devices")
+      .doc<Auto>("farmEST17a")
+      .valueChanges();
     this.autoButton.subscribe((state) => {
       this.isChecker = state.autoIsrunning;
-      console.log('On value Observe:' + this.isChecker);
-      
-  });
-
-}
-async buttonMotor($event) {
-
-  const name = this.afs.collection('devices').doc('farmEST17a');
-
-  try {
-
-    console.log('in autoMotor:' + this.isChecker);
-
-
-    await name.update({
-      autoIsrunning: !this.isChecker,
+      console.log("On value Observe:" + this.isChecker);
     });
-  } catch (error) {
-    // catch error
   }
-}
-async autoTime(value){
- 
+  async buttonMotor($event) {
+    const name = this.afs.collection("devices").doc("farmEST17a");
 
-      this.afs
-              .collection('devices').doc('farmEST17a')
-              .update({
-              min: value.min,
-              max:value.max
-              })
-              .then(() => {
-                this.successMessage = 'Auto Set Successfully.';
-              });
+    try {
+      console.log("in autoMotor:" + this.isChecker);
 
+      await name.update({
+        autoIsrunning: !this.isChecker,
+      });
+    } catch (error) {
+      // catch error
     }
-
->>>>>>> fc5fca2d104568ef83ffbac55d51b5f7c0d92f41
+  }
+  async autoTime(value) {
+    this.afs
+      .collection("devices")
+      .doc("farmEST17a")
+      .update({
+        min: value.min,
+        max: value.max,
+      })
+      .then(() => {
+        this.successMessage = "Auto Set Successfully.";
+      });
+  }
 }
